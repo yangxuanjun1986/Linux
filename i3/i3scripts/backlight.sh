8e01301367dd052f03ed7a89b7af1fa050d9bedf
yangxuanjun86 <yangxuanjun@hotmail.com>
2019-03-08 14:17:30 +0800

    config

---
+++ i3/i3scripts/backlight.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+current_brightness=$(light -G)
+
+case $BLOCK_BUTTON in
+  4) /fast_files/git_repos/custum_scripts/changebacklight.sh up ;;
+  5) /fast_files/git_repos/custum_scripts/changebacklight.sh down ;;
+esac
+
+echo "$current_brightness%"
+
+# cat /sys/class/backlight/intel_backlight/actual_brightness | sed 's/^/100*/' | sed 's/$/\/937/' | bc | sed 's/$/%/'
